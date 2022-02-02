ce5e908e4ee61affe2e6346467c4d1f9565a2da9
Murad Buyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-02-02 20:49:26 +0100

    added session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 // import App from "next/app";
 import type { AppProps /*, AppContext */ } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 // Only uncomment this method if you have blocking data requirements for
