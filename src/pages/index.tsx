084d90636f50847704ac0c0d4956857b707d4d10
Murad Buyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-02-02 20:49:35 +0100

    created homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,18 @@
-import Layout from '@/components/Layout';
-import TimelineEntry from '@/components/TimelineEntry';
-import { getEntries } from '@/utils/timelineQueries';
-import { GetStaticProps } from 'next';
+import Layout from '@/components/common/Layout/Layout';
+import React from 'react';
 
-const index = (entries) => {
-
-	return (
-		<Layout>
-			<section>
-				<TimelineEntry title='The night of naming' createdOn='test' />
-			</section>
-		</Layout>
-	);
-}
-
-export const getStaticProps: GetStaticProps = async (ctx) => {
-	const entries = await getEntries()
-
-	return {
-		props: {
-			entries: entries
-		}
-	}
-}
+const index = () => {
+  return (
+    <Layout>
+      <p className="text-neutral-900 dark:text-white">
+        The Thumos Archives is a fan project made by <a className="text-sky-600 font-semibold" href="https://github.com/muradbuyukasik">Murad</a> for the High Thumos men's group founded by Elisha Long.
+      </p>
+      <div className="flex flex-col mt-6 space-y-4">
+        <a className="text-center text-2lg text-white font-bold bg-red-800 hover:bg-red-900 shadow-md rounded-md py-2" href="https://www.youtube.com/c/ElishaLong">Subscribe to Elisha Long on YouTube</a>
+        <a className="text-center text-2lg text-white font-bold bg-red-800 hover:bg-red-900 shadow-md rounded-md py-2" href="https://www.patreon.com/HighThumos">Join the High Thumos Discover server</a>
+      </div>
+    </Layout>
+  )
+};
 
 export default index;
