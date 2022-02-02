0846e85eeca338a70c3e8dab12368fa705d6b38f
Murad Buyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-02-02 20:51:06 +0100

    created timeline entry card

--- src/components/TimelineEntry.tsx
+++ src/components/TimelineEntry.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const TimelineEntry = ({ title, createdOn }: Props) => {
     return (
-        <article className="bg-white dark:bg-slate-900 rounded-lg px-4 py-5 drop-shadow-md dark:border dark:border-slate-600">
-            <h2 className="text-slate-800 dark:text-white text-lg font-bold">{title}</h2>
-            <small className="text-slate-500 dark:text-slate-400 text-sm">{createdOn}</small>
+        <article className="bg-white dark:bg-zinc-800 rounded-lg px-4 py-5 shadow-md dark:border dark:border-zinc-900">
+            <h2 className="text-neutral-900 dark:text-white text-lg font-bold">{title}</h2>
+            <small className="text-neutral-700 dark:text-slate-400 text-sm">{createdOn}</small>
         </article>
     )
 };
